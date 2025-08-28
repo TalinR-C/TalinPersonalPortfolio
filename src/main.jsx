0ac732887d8259f52d860436791f1fea8411907b
Talin <elev8ed.app1@gmail.com>
2025-08-28 18:14:01 +1000

    Update Main error

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
+// Remove the require statement since we don't need it for now
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router basename="/TalinPersonalPortfolio">
